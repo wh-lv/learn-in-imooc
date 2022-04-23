41856482c9e9a7637689f8eaf5e66aae8f53ca8b
wh-lv <palomawhl@163.com>
2022-04-23 21:18:42 +0800

    browserslistrc工作流程

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -6,3 +6,25 @@ import "./js/login"
 console.info(sum(10, 20))
 console.info(square(10))
 console.info(getInfo())
+
+/**
+ * 1.工程化
+ * 2.兼容性：CSS、JS
+ * 3.如何实现兼容
+ * 4.到底要兼容哪些平台
+ *  caniuse.com
+ * 
+ * >1% // 市场占有率大于 1%
+ * default
+ * dead
+ * last 2 version 浏览器最新的两个版本
+ * 
+ * 可以写到 package.json 中
+ * "browserslist": [
+    ">1%",
+    "last 2 version",
+    "not dead"
+  ]
+
+  或者新建一个 .browserslistrc 文件
+ */
